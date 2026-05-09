dde8730a95c82c65118e45ccca6df44f59fb4968
Mohammad Ishfaqul Islam <sajid.ishfaq.bd@gmail.com>
2026-05-09 22:55:37 +0600

    Update submission-1.cpp

--- Data Structures & Algorithms/Dynamic Programming/dp problems (2D)/longest-common-subsequence/submission-1.cpp
+++ Data Structures & Algorithms/Dynamic Programming/dp problems (2D)/longest-common-subsequence/submission-1.cpp
@@ -6,11 +6,16 @@ class Solution {
         vector<int> prev(n + 1, 0);
         vector<int> curr(n + 1, 0);
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (text1[i - 1] == text2[j - 1]) {
+        for (int i = 1; i <= m; i++) 
+        {
+            for (int j = 1; j <= n; j++) 
+            {
+                if (text1[i - 1] == text2[j - 1]) 
+                {
                     curr[j] = prev[j - 1] + 1;
-                } else {
+                } 
+                else 
+                {
                     curr[j] = max(prev[j], curr[j - 1]);
                 }
             }
