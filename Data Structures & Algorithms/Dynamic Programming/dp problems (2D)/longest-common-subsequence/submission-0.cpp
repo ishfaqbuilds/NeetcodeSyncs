a9e635651e12a65a29a4576ebc164eaf1d1632f6
Mohammad Ishfaqul Islam <sajid.ishfaq.bd@gmail.com>
2026-05-09 22:54:42 +0600

    Update NeetcodeSyncs repository

--- Data Structures & Algorithms/Dynamic Programming/dp problems (2D)/longest-common-subsequence/submission-0.cpp
+++ Data Structures & Algorithms/Dynamic Programming/dp problems (2D)/longest-common-subsequence/submission-0.cpp
@@ -9,13 +9,13 @@ public:
         {
             for (int j = 1; j <= n; j++)
             {
-                if (text1[i - 1] == text2[j - 1])
+                if (text1[i - 1] == text2[j - 1]) 
                 {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dp[i][j] = dp[i - 1][j - 1] + 1; // Match
                 }
                 else
                 {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]); // Didn't Match
                 }
             }
         }
