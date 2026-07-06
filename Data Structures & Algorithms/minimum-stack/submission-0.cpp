f78978639d332240e06c490daae85a9597500319
Mohammad Ishfaqul Islam <sajid.ishfaq.bd@gmail.com>
2026-07-06 13:33:17 +0600

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,36 @@
+class MinStack {
+   public:
+    stack<int> st;
+    stack<int> minStack;
+    MinStack() {
+
+    }
+
+    void push(int val) 
+    {
+        st.push(val);
+        if (minStack.empty() || val <= minStack.top()) 
+        {
+            minStack.push(val);
+        }
+    }
+
+    void pop() 
+    {
+        if (st.top() == minStack.top()) 
+        {
+            minStack.pop();
+        }
+        st.pop();
+    }
+
+    int top() 
+    {
+        return st.top();
+    }
+
+    int getMin() 
+    {
+        return minStack.top();
+    }
+};
